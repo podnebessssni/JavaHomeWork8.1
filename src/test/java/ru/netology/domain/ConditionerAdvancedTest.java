2d4215cc3ea945cb06f779f2af018302d19f1cc5
Oleg Georgiyevski <oleqg@yahoo.com>
2020-07-01 15:01:33 +0300

    Initial commit

---
+++ src/test/java/ru/netology/domain/ConditionerAdvancedTest.java
@@ -0,0 +1,18 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ConditionerAdvancedTest {
+    @Test
+    public void shouldGetAndSet() {
+        ConditionerAdvanced conditioner = new ConditionerAdvanced();
+        String expected = "Кондишн";
+
+        assertNull(conditioner.getName());
+        conditioner.setName(expected);
+        assertEquals(expected, conditioner.getName());
+    }
+}
